2b5fe8bf74313366aa4f636fb1f46d788519a23c
umitkose <umitkose@turksat.com.tr>
2023-07-17 10:09:23 +0300

    BinarySearch is added
    Signed-off-by: umitkose <umitkose@turksat.com.tr>

--- src/main/java/com/umiitkose/array/easy/quiz704/BinarySearch.java
+++ src/main/java/com/umiitkose/array/easy/quiz704/BinarySearch.java
@@ -23,3 +23,22 @@ public class BinarySearch {
         return -1;
     }
 }
+
+/**
+ * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ */
