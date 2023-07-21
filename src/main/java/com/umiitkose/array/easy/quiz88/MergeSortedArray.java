b00c516f1cb68f7900327afc1bc94026825d7000
umitkose <umitkose@turksat.com.tr>
2023-07-21 18:08:42 +0300

    MergeSortedArray is added
    Signed-off-by: umitkose <umitkose@turksat.com.tr>

---
+++ src/main/java/com/umiitkose/array/easy/quiz88/MergeSortedArray.java
@@ -0,0 +1,68 @@
+package com.umiitkose.array.easy.quiz88;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        mergeSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int nums1Length = m - 1, nums2Length = n - 1, sizeTotal = n + m - 1;
+
+        while (nums2Length >= 0) {
+            if (nums2Length >= 0 && nums1[nums1Length] > nums2[nums2Length]) {
+                nums1[sizeTotal--] = nums1[nums1Length--];
+            }
+
+            if (nums1Length >= 0 && nums1[nums1Length] <= nums2[nums2Length]) {
+                nums1[sizeTotal--] = nums2[nums2Length--];
+            }
+
+        }
+
+
+    }
+}
+
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * <p>
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * <p>
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ * Example 2:
+ * <p>
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * Explanation: The arrays we are merging are [1] and [].
+ * The result of the merge is [1].
+ * Example 3:
+ * <p>
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Output: [1]
+ * Explanation: The arrays we are merging are [] and [1].
+ * The result of the merge is [1].
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ * <p>
+ * <p>
+ * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+ */
